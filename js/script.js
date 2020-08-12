8dfbed2f38d8cd6df6668f69cf662dd21cddeb33
Dirk van Goethem <vangoethem.dirk@hotmail.com>
2020-08-12 14:10:32 +0200

    deleted duplicated code

--- js/script.js
+++ js/script.js
@@ -83,3 +83,21 @@ const x = setInterval(() => {
     totalSeconds--;
     tempSeconds = totalSeconds;
 }, 1000);
+
+// Cards
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
